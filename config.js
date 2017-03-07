e926bd1b10a09bf0bc1054ec6dfe131c8275495b
Tricia Katz <tricia.katz@mutualmobile.com>
2017-03-07 15:48:26 -0600

    Update config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'EAAK7uiSlaCUBANRGrr2ymBsWP0howvRwnN4tZAfo7JOEd3ZAI1inqm38orvxqm90yw9ZBMh19KHy1nqSKWKpWoz6ZBQgKS7e6g9Rd9MZCjFQZAeucJWctJZC1oLfP8KJggwB1zyFcbCnLdQuwc9SSVWCY2ojYIIrEtFGB3FZBggfRN0ulzPN4gJI',
+    FB_VERIFY_TOKEN: 'mm-agency-token',
+    API_AI_CLIENT_ACCESS_TOKEN: '2311c296fa134f02a1ab066b36cbeedf',
+    FB_APP_SECRET: 'f36b51cd335b1ef738adb846ed491d4f',
+    SERVER_URL: "http://tranquil-springs-76921.herokuapp.com/",
 };
