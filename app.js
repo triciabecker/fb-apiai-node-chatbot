96920a7e5739418c2b7ca6a33dd468f7943d4558
Tricia Katz <tricia.katz@mutualmobile.com>
2017-03-07 16:11:56 -0600

    add sendgrid

--- app.js
+++ app.js
@@ -205,8 +205,27 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 		}
 }
 
-function sendEmailMessage(subject, content) {
+function sendEmailMessage(emailSubject, content) {
+	var helper = require('sendgrid').mail;
+	  
+	from_email = new helper.Email("tricia.katz@mutualmobile.com");
+	to_email = new helper.Email("tricia.katz@mutualmobile.com");
+	subject = emailSubject;
+	content = new helper.Content("text/html", content);
+	mail = new helper.Mail(from_email, subject, to_email, content);
 
+	var sg = require('sendgrid')(config.SENDGRID_API_KEY);
+	var request = sg.emptyRequest({
+	  method: 'POST',
+	  path: '/v3/mail/send',
+	  body: mail.toJSON()
+	});
+
+	sg.API(request, function(error, response) {
+	  console.log(response.statusCode);
+	  console.log(response.body);
+	  console.log(response.headers);
+	})
 
 }
 
--- config.js
+++ config.js
@@ -4,4 +4,5 @@ module.exports = {
     API_AI_CLIENT_ACCESS_TOKEN: '2311c296fa134f02a1ab066b36cbeedf',
     FB_APP_SECRET: 'f36b51cd335b1ef738adb846ed491d4f',
     SERVER_URL: "http://tranquil-springs-76921.herokuapp.com/",
+    SENDGRID_API_KEY: "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",
 };
